774be86b0750b01ee0eccb58fca530b103566e44
mtdx <ms2asdk@gmail.com>
2017-07-19 12:08:24 -0700

    des encryption

---
+++ DES/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace CryptographyInDotNet
+{
+    class Program
+    {
+        static void Main()
+        {
+            var des = new DesEncryption();
+            var key = des.GenerateRandomNumber(8);
+            var iv = des.GenerateRandomNumber(8);
+            const string original = "Text to encrypt";
+
+            var encrypted = des.Encrypt(Encoding.UTF8.GetBytes(original), key, iv);
+            var decrypted = des.Decrypt(encrypted, key, iv);
+
+            var decryptedMessage = Encoding.UTF8.GetString(decrypted);
+
+            Console.WriteLine();
+            Console.WriteLine("Original Text = " + original);
+            Console.WriteLine("Encrypted Text = " + Convert.ToBase64String(encrypted));
+            Console.WriteLine("Decrypted Text = " + decryptedMessage);
+            
+            Console.ReadLine();
+        }
+    }
+}
